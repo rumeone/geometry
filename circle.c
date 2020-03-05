1b0a787f279c0ea3b8f85bccbe8117d9d848c375
rumeone <yuaroslav_29@mail.ru>
2020-03-05 12:54:32 +0700

    edit circle.c

--- circle.c
+++ circle.c
@@ -1,15 +1,14 @@
 #include "point.h"
 #include <stdio.h>
 #include <math.h>
-#include <stdio.h>
 
-void enter_coordinate_circle(circle *c) {
+void enter_coordinate_circle(circle* c) {
   printf("Введите координаты окружности: ");
   scanf("%d %d", &c->A.x, &c->A.y );
   printf("Введите радиус окружности: ");
   scanf("%d", &c->r);
 }
 void print_circle(circle *c, FILE *f) {
-fprintf(f, "circle((%d:%d),  %d)\n", c->A.x, c->A.y, C->r);
-printf("circle((%d;%d), %d)\n", c->A.x, c->A.y, c->R);
+fprintf(f, "circle((%d:%d),  %d)\n", c->A.x, c->A.y, c->r);
+printf("circle((%d;%d), R=%d)\n", c->A.x, c->A.y, c->r);
 }
