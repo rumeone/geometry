a163f2af16384004a6ccebca3cd9662fd18c4c48
rumeone <yuaroslav_29@mail.ru>
2020-03-05 12:55:16 +0700

    edit point.h

--- point.h
+++ point.h
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef point {
+typedef struct point {
   int x;
   int y;
 
-} point_t;
+ } point_t;
 
-typedef circle {
+typedef struct circle {
   point_t A;
   int r;
 } circle;
 
 void enter_coordinate_circle(circle* c);
 void print_circle(circle *c, FILE *f);
+
