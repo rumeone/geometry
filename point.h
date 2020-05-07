2d9d3db2cc7d91a79d29e58c02f6c329c88f0d9d
rumeone <yuaroslav_29@mail.ru>
2020-05-07 23:58:44 +0700

    Add function types

--- point.h
+++ point.h
@@ -8,10 +8,22 @@ typedef struct point {
  } point_t;
 
 typedef struct circle {
-  point_t A;
+  point_t C;
   int r;
 } circle;
 
+typedef struct triangle {
+	point_t T[4];
+}triangle;
+
 void enter_coordinate_circle(circle* c);
 void print_circle(circle *c, FILE *f);
+float perimeter_circle(circle* c, FILE* f);
+float area_circle(circle* c, FILE* f);
+void enter_triangle_coordinate(triangle* t);
+void print_triangle(triangle* t, FILE* f);
+int check_triangle(triangle* t, FILE* f);
+float perimeter_triangle(triangle* t, FILE* f);
+float area_triangle(triangle* t, FILE* f);
+
 
